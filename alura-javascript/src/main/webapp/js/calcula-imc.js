57203e0a9b4b4d7850766606e9f5121e720a05d5
KAZUMITSU THIAGO BUGS WAKASSA <kazumitsu.wakassa@caixa.gov.br>
2018-08-14 18:58:02 -0300

    Aula 5 completa

--- alura-javascript/src/main/webapp/js/calcula-imc.js
+++ alura-javascript/src/main/webapp/js/calcula-imc.js
@@ -2,49 +2,45 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes.length)
+
 for (var i = 0; i < pacientes.length; i++) {
 
-	var paciente = pacientes[i];
+    var paciente = pacientes[i];
 
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-	var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-	var pesoEhValido = true;
-	var alturaEhValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-	if (peso <= 0 || peso >= 1000) {
-		console.log("Peso inválido!");
-		pesoEhValido = false;
-		tdImc.textContent = "Peso inválido";
-		paciente.classList.add("paciente-invalido");
-	}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-	if (altura <= 0 || altura >= 3.00) {
-		console.log("Altura inválida!");
-		alturaEhValida = false;
-		tdImc.textContent = "Altura inválida";
-		paciente.classList.add("paciente-invalido");
-	}
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
 
-	if (alturaEhValida && pesoEhValido) {
-		tdImc.textContent = calculaImc(peso, altura);
-	}
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
 }
 
-function calculaImc(peso, altura){
+function calculaImc(peso, altura) {
     var imc = 0;
-
     imc = peso / (altura * altura);
 
     return imc.toFixed(2);
 }
-
-titulo.addEventListener("click", function() {
-	console.log("Olha só posso chamar uma função anônima.")
-});
